a8ff7aff659c3db2b9a366913c4bd1d85d463a2c
Unknown <adh416@nyu.edu>
2018-09-27 14:15:10 -0400

    Project Commit

---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour {
+
+    GameObject mainCamera;
+    GameObject carriedObject;
+    public bool carrying;
+    public float distance;
+    public float pickupDistance = 3;
+    public float smoothing;
+
+
+	
+	void Start () {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+	}
+	
+	
+	void Update () {
+        if (carrying)
+        {
+            carry(carriedObject);
+            checkDrop();
+        }
+        else
+        {
+            pickup();
+        }
+	}
+    
+    void carry(GameObject obj)
+    {
+        if(obj != null)
+        {
+            obj.transform.position = Vector3.Lerp(obj.transform.position,
+            mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smoothing);
+        }
+        
+    }
+
+    void pickup()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.E))
+        {
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+
+            Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                //if (hit.transform.gameObject.tag == "Star")
+                //{
+                //    Debug.Log("hit it");
+                //    Debug.Log(Vector3.Distance(hit.transform.position, mainCamera.transform.position));
+                //}
+
+                Pickupable pickupable = hit.collider.GetComponent<Pickupable>();
+                if(pickupable != null && Vector3.Distance(pickupable.gameObject.transform.position, mainCamera.transform.position) < pickupDistance)
+                {
+                    carrying = true;
+                    carriedObject = pickupable.gameObject;
+                    pickupable.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                }
+            }
+        }
+    }
+
+    void checkDrop()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.E))
+        {
+            dropObject();
+        }
+    }
+
+    void dropObject()
+    {
+        if (carriedObject != null)
+        {
+            carrying = false;
+            carriedObject.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            carriedObject = null;
+        }
+    }
+}
