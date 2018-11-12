fef77c9edd177bb12b0e02db7045a0b4f01a144b
Unknown <adh416@nyu.edu>
2018-11-12 14:48:49 -0500

    Scripts and House
    More script updates. added in house blockout

--- Assets/Main Scripts/PlacementLocation.cs
+++ Assets/Main Scripts/PlacementLocation.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlacementLocation : MonoBehaviour {
+public class PlacementLocation : MonoBehaviour
+{
 
+	public Prop targetProp;
+	
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +16,18 @@ public class PlacementLocation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void CheckCondition()
+	{
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (targetProp != null)
+		{
+			
+		}
 	}
 }
