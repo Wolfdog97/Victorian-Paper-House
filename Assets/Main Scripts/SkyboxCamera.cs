f6532a0c2c43ef27c2801ec7fb72ac0032eba91d
Unknown <adh416@nyu.edu>
2019-01-07 12:26:46 -0500

    Winter Milestone build

---
+++ Assets/Main Scripts/SkyboxCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxCamera : MonoBehaviour
+{
+	private Camera _camera;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		_camera.fieldOfView = Camera.main.fieldOfView;
+		transform.rotation = Camera.main.transform.rotation;
+	}
+}
