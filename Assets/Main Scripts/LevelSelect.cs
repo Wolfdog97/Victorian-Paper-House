6b6972c1179079f2ed3d94356ffae537c1604b9d
Unknown <adh416@nyu.edu>
2019-02-09 12:08:27 -0500

    Now Entering Development Hell

--- Assets/Main Scripts/LevelSelect.cs
+++ Assets/Main Scripts/LevelSelect.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
-public class LevelSelect : MonoBehaviour {
+public class LevelSelect : MonoBehaviour
+{
 
+	public GameObject loadingScreen;
+	public Slider slider;
+	public TextMeshProUGUI progressText;
+	
 	public void LoadLevelName(string levelName)
 	{
 		SceneManager.LoadScene(levelName);
 	}
 	
-	public void LoadLevelIndex(int levelIndex)
+	public void LoadLevelIndex(int sceneIndex)
 	{
-		SceneManager.LoadScene(levelIndex);
+		//SceneManager.LoadScene(sceneIndex);
+		StartCoroutine(LoadAsynchronously(sceneIndex));
 	}
 
 	public void LoadNextLevel()
@@ -23,5 +31,23 @@ public class LevelSelect : MonoBehaviour {
 	public void LoadSceneZero()
 	{
 		SceneManager.LoadScene(0);
+	}
+
+	IEnumerator LoadAsynchronously(int sceneIndex)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+		loadingScreen.SetActive(true);
+		
+		while ( (!operation.isDone))
+		{
+			float progress = Mathf.Clamp01(operation.progress / .9f);
+
+			slider.value = progress;
+			progressText.text = progress * 100f + "%";
+			
+			Debug.Log(progress);
+			yield return null;
+		}
 	}
 }
